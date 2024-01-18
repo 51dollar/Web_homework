0d1ebc844ecc00834f4fb60bf6458e24adf4b8a1
Borushko Dima <borushkoddima@mail.ru>
2024-01-18 15:50:11 +0300

    database connection

--- MVC/Program.cs
+++ MVC/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+
 namespace MVC
 {
 	public class Program
@@ -8,8 +11,11 @@ namespace MVC
 
 			// Add services to the container.
 			builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")
+                ));
 
-			var app = builder.Build();
+            var app = builder.Build();
 
 			// Configure the HTTP request pipeline.
 			if (!app.Environment.IsDevelopment())
