a9834148287f0796e85f90275ea9bd3f77d7f3df
Borushko Dima <borushkoddima@mail.ru>
2024-01-18 15:51:04 +0300

    add-migration AddFriendsToDataBase Update-Database

---
+++ MVC/Migrations/20240118124430_AddFriendsToDataBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFriendsToDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "friends",
+                columns: table => new
+                {
+                    FriendID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FriendName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_friends", x => x.FriendID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "friends");
+        }
+    }
+}
