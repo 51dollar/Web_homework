63c94925b5271e4440e593611e9488dd2c3c8bff
Borushko Dima <borushkoddima@mail.ru>
2024-01-18 16:21:00 +0300

    add a views list of friends

--- MVC/Controllers/FriendController.cs
+++ MVC/Controllers/FriendController.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Data;
+using MVC.Models;
 
 namespace MVC.Controllers
 {
     public class FriendController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public FriendController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Friend> objFrindsList = _db.friends.ToList();
+            return View(objFrindsList);
         }
     }
 }
