ee6ff4a3bcf5fb4264d335543015272fdc44387d
Borushko Dima <borushkoddima@mail.ru>
2024-01-18 15:21:53 +0300

    add controllers FriendControlle.cs and models Friend.cs

---
+++ MVC/Models/Friend.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models
+{
+    public class Friend
+    {
+        [Key]
+        public int FriendID { get; set; }
+        [Required]
+        public string? FriendName { get; set; }
+        public string? Place { get; set; }
+    }
+}
