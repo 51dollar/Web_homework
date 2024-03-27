b6a2e9aa6aeedf5e27a62f34a0d577217c5cc4b6
Borushko Dima <borushkoddima@mail.ru>
2024-03-27 11:57:52 +0300

    Сhange home pageHomework: Data Validation
    add [MaxLength(n)]
    add [Required(ErrorMessage = "_")]

--- MVC/Models/Friend.cs
+++ MVC/Models/Friend.cs
@@ -6,11 +6,15 @@ namespace MVC.Models
     public class Friend
     {
         [Key]
-        public Guid FriendID { get; set; }
-        [Required]
+        public required Guid FriendID { get; set; }
+
         [DisplayName("Name")]
+        [MaxLength(100, ErrorMessage = "Maximum length for the Names is 100 characters")]
+        [Required(ErrorMessage = "Username not specified")]
         public string? FriendName { get; set; }
-		[Required]
-		public string? Place { get; set; }
+
+        [MaxLength(25, ErrorMessage = "Maximum length for the Place is 25 characters")]
+        [Required(ErrorMessage = "user place not specified")]
+        public string? Place { get; set; }
     }
 }
